41b7bbbf0ecec722a1ae8940e79595f9d9836b58
Javontay McElroy <dinomightnetwork@gmail.com>
2019-05-20 23:36:50 -0500

    Set up axios.all()

--- tvmv-react-app/src/App.js
+++ tvmv-react-app/src/App.js
@@ -1,21 +1,50 @@
+//--------------DEPENDANCIES-------------------//
 import React from 'react';
+import axios from 'axios';
+//--------------STYLES-------------------//
 import './App.css';
-
-class App extends React.Component{
-  constructor(){
+//--------------CLASS COMPONENT-------------------//
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
-      data: []
+      popularMovie: [],
+      popularShow: []
+    };
+  }
+  //--------------GET MOST POPULAR MOVIE-------------------//
+  componentDidMount() {
+    function getPopularMovie() {
+      return axios.get(
+        'https://api.themoviedb.org/3/movie/popular?api_key=6d9a91a4158b0a021d546ccd83d3f52e&language=en-US&page=1'
+      );
     }
+    function getPopularShows() {
+      return axios.get(
+        'https://api.themoviedb.org/3/tv/popular?api_key=6d9a91a4158b0a021d546ccd83d3f52e&language=en-US&page=1'
+      );
+    }
+    axios
+      .all([getPopularMovie(), getPopularShows()])
+      .then(res => {
+        console.log(res);
+        this.setState({
+          popularMovie: res[0].data.results,
+          popularShow: res[1].data.results
+        });
+      })
+      .catch(err => console.log(err));
   }
 
-  render(){
-      return (
-        <div className="App">
+  render() {
+    return (
+      <>
+        <div className='App'>
           <h1>Project TVMV</h1>
         </div>
-      );
-   }
+      </>
+    );
+  }
 }
 
 export default App;
