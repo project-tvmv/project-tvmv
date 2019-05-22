78638466915399d9c4cc42324eacdad67a6923f4
Javontay McElroy <dinomightnetwork@gmail.com>
2019-05-21 22:26:16 -0500

    Add sub hero.

--- tvmv-react-app/src/components/sections/SubHero.js
+++ tvmv-react-app/src/components/sections/SubHero.js
@@ -8,13 +8,31 @@ const SubHero = ({ popularMovies, popularShows }) => {
     <div className='subhero-container'>
       <>
         {popularMovies.splice(1, 1).map((popularMovie, index) => (
-          <img
-            src={
-              'http://image.tmdb.org/t/p/original' + popularMovie.backdrop_path
-            }
-            alt={popularMovie.title}
-            className='subhero-image-left'
-          />
+          <>
+            <h1 className='sub-hero-movie-title'>{popularMovie.title}</h1>
+            <p className='subhero-movie-desc'>{popularMovie.overview}</p>
+            <img
+              src={
+                'http://image.tmdb.org/t/p/original' +
+                popularMovie.backdrop_path
+              }
+              alt={popularMovie.title}
+              className='subhero-image-left'
+            />
+          </>
+        ))}
+        {popularShows.splice(3, 1).map((popularShow, index) => (
+          <>
+            <h1 className='sub-hero-show-title'>{popularShow.name}</h1>
+            <p className='subhero-show-desc'>{popularShow.overview}</p>
+            <img
+              src={
+                'http://image.tmdb.org/t/p/original' + popularShow.backdrop_path
+              }
+              alt={popularShow.name}
+              className='subhero-image-right'
+            />
+          </>
         ))}
       </>
     </div>
