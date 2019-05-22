6064fc3842a3b09c7ee5b5f6b8e264b646a19062
Javontay McElroy <dinomightnetwork@gmail.com>
2019-05-22 17:46:14 -0500

    Got data for SIngle Movie!!!

--- tvmv-react-app/src/components/SingleMovie.js
+++ tvmv-react-app/src/components/SingleMovie.js
@@ -1,20 +1,33 @@
 //--------------DEPENDANCIES-------------------//
 import React from 'react';
 import { Component } from 'react';
+import axios from 'axios';
 //--------------CLASS COMPONENT-------------------//
 class SingleMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
       id: this.props.match.params.id,
-      popularMovies: this.props.popularMovies,
-      newMovies: this.props.newMovies
+      movie: []
     };
   }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${
+          this.state.id
+        }?api_key=6d9a91a4158b0a021d546ccd83d3f52e&language=en-US`
+      )
+      .then(res => this.setState({ movie: res.data }))
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const movie = this.state.movie;
     return (
       <div className='single-movie-containter'>
-        <h1>Welcome to the Movie</h1>
+        <h1>{movie.title}</h1>
       </div>
     );
   }
