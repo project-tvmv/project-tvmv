319831d0606a94eef4b68b31bd9407497f6c76ed
amlane <twelvexstring@gmail.com>
2019-05-25 00:02:57 -0500

    got TV and movies linked

--- tvmv-react-app/src/components/SearchResults.js
+++ tvmv-react-app/src/components/SearchResults.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SearchResults(props){
 
@@ -9,19 +10,19 @@ function SearchResults(props){
             {props.item.title ? <p>{props.item.title}</p> : <p>{props.item.name}</p>}
 
             {props.item.poster_path ? 
+                <Link to={props.item.media_type === "movie" ? `/movie/${props.item.id}` : `/television/${props.item.id}` }>
                 <img 
                 src={`http://image.tmdb.org/t/p/original${props.item.poster_path}`} 
                 alt={props.item.name}
                 className="search-poster"
                 />
+                </Link>
              : 
                 <img src="https://previews.123rf.com/images/leodekol/leodekol1705/leodekol170500001/77299588-retro-movie-film-frame-.jpg"
                 className="search-poster"
                 alt="no poster available"
                 />
             }
-
-            <p>{props.item.vote_average} average score</p>
         </div>
     )
 }
