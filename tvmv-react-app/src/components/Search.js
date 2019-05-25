d7fad1c2e6e8829a7ed791eb844d7027f7bea79b
amlane <twelvexstring@gmail.com>
2019-05-24 22:24:29 -0500

    got search pulling dynamically

--- tvmv-react-app/src/components/Search.js
+++ tvmv-react-app/src/components/Search.js
@@ -6,12 +6,12 @@ import Axios from 'axios';
 //---------------Syles-------------------//
 class Search extends Component {
   state = {
-    search: "frozen"
+    search: ""
   };
 
-  componentDidMount() {
+  fetchSearch(search) {
     Axios
-    .get(`https://api.themoviedb.org/3/search/multi?api_key=6d9a91a4158b0a021d546ccd83d3f52e&language=en-US&language=en-US&query={this.state.search}&page=1&include_adult=false`)
+    .get(`https://api.themoviedb.org/3/search/multi?api_key=6d9a91a4158b0a021d546ccd83d3f52e&language=en-US&language=en-US&query=${search}&page=1&include_adult=false`)
     .then(res => {
       console.log("response:", res)
     })
@@ -20,15 +20,32 @@ class Search extends Component {
     })
   }
 
+  changeHandler = e => {
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.search)
+    this.fetchSearch(this.state.search);
+  }
+
 
   render() {
     return (
       <div className="search-page">
         <h1>Welcome to Search</h1>
         <form className="search-box">
-          <button>+</button>
+          <button
+          onClick={this.onSubmit}
+          >+</button>
           <input
           placeholder="WHAT CAN WE HELP YOU FIND TODAY?"
+          name="search"
+          value={this.state.search}
+          onChange={this.changeHandler}
           />
         </form>
       </div>
