7d149b4225a70b2cde3af33c87623d39364336bd
cecil john tantay <cjbtantay@gmail.com>
2019-05-25 23:37:29 -0700

    win

--- tvmv-react-app/src/components/MovieContent/SingleMovie.js
+++ tvmv-react-app/src/components/MovieContent/SingleMovie.js
@@ -36,25 +36,36 @@ class SingleMovie extends Component {
       .then(res => this.setState({ movie: res.data }))
       .catch(err => console.log(err));
 
-      //------FAVORITES----//
+    //------FAVORITES----//
 
-      if (!localStorage.favoriteMovies) {
-        let favoriteMovies = []
-        favoriteMovies.push(JSON.parse(localStorage.getItem('favoriteMovies')));
-        localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
-      }
+    if (!localStorage.favoriteMovies) {
+      let favoriteMovies = [];
+      favoriteMovies.push(JSON.parse(localStorage.getItem('favoriteMovies')));
+      localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+    }
   }
 
-    // -----------------------------FAVORITES---------------------------------- //
+  // -----------------------------FAVORITES---------------------------------- //
 
   addMovietoLocalFavorites = (id, poster_path) => {
     // grabs current items and spreads them to a new array. Add new item after it.
     let favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies'));
-    favoriteMovies.push({id, poster_path})
+    favoriteMovies.push({ id, poster_path });
     // removes any null in array
-    const filteredMovies = favoriteMovies.filter(item => item !== null)
+    const filteredMovies = favoriteMovies.filter(item => item !== null);
+    const result = [];
+    const map = new Map();
+    for (const item of filteredMovies) {
+      if (!map.has(item.id)) {
+        map.set(item.id, true);
+        result.push({
+          id: item.id,
+          poster_path: item.poster_path
+        });
+      }
+    }
 
-    localStorage.setItem('favoriteMovies', JSON.stringify(filteredMovies))
+    localStorage.setItem('favoriteMovies', JSON.stringify(result));
 
     /**
      * To grab items, you must first JSON.parse(localStorage.getItem('favoriteMovies'))
@@ -63,7 +74,7 @@ class SingleMovie extends Component {
      *  */
 
     // console log local storage to see what's inside
-    console.log(JSON.parse(localStorage.favoriteMovies))
+    console.log(JSON.parse(localStorage.favoriteMovies));
   };
 
   render() {
@@ -72,46 +83,49 @@ class SingleMovie extends Component {
     const movie = this.state.movie;
     //--------------END OF DECONSTRUCTING-------------------//
     return (
-      <div className='single-page-containter'>
+      <div className="single-page-containter">
         {/* //--------------HERO SECTION-------------------// */}
         <img
           src={back}
-          className='hero-back'
-          alt='back'
+          className="hero-back"
+          alt="back"
           onClick={this.props.history.goBack}
         />
-        <img src={star} 
-        className='hero-star' 
-        alt='star' 
-        onClick={()=> (
-          this.addMovietoLocalFavorites(this.state.movie.id, 
-          this.state.movie.poster_path)
-          )}   
+        <img
+          src={star}
+          className="hero-star"
+          alt="star"
+          onClick={() =>
+            this.addMovietoLocalFavorites(
+              this.state.movie.id,
+              this.state.movie.poster_path
+            )
+          }
         />
-        <div className='single-page-hero-info'>
-          <h1 className='single-page-hero-title'>{movie.title}</h1>
-          <div className='movie-details'>
-            <p className='movie-details-text'>
+        <div className="single-page-hero-info">
+          <h1 className="single-page-hero-title">{movie.title}</h1>
+          <div className="movie-details">
+            <p className="movie-details-text">
               {moment(movie.release_date, 'YYYY-MM-DD').format('MMMM Do YYYY')}
             </p>
-            <p className='line-breaks'>|</p>
-            <p className='movie-details-text'>{movie.vote_average}</p>
-            <p className='line-breaks'>|</p>
-            <p className='movie-details-text'>Runtime: {movie.runtime} mins</p>
+            <p className="line-breaks">|</p>
+            <p className="movie-details-text">{movie.vote_average}</p>
+            <p className="line-breaks">|</p>
+            <p className="movie-details-text">Runtime: {movie.runtime} mins</p>
           </div>
-          <p className='single-page-hero-desc'>{movie.overview}</p>
-          <div className='single-page-button-flex'>
-            <Link to={`/playing/${this.state.id}`} className='button-links'>
-              <button className='watch-movie'>Watch movie</button>
+          <p className="single-page-hero-desc">{movie.overview}</p>
+          <div className="single-page-button-flex">
+            <Link to={`/playing/${this.state.id}`} className="button-links">
+              <button className="watch-movie">Watch movie</button>
             </Link>
-            <Link to={`/trailer/${this.state.id}`} className='button-links'>
-              <button className='watch-trailer'> Watch trailer </button>
+            <Link to={`/trailer/${this.state.id}`} className="button-links">
+              <button className="watch-trailer"> Watch trailer </button>
             </Link>
           </div>
         </div>
         <img
           src={'http://image.tmdb.org/t/p/original' + movie.backdrop_path}
-          className='full-hero'
+          className="full-hero"
           alt={movie.title}
           onError={this.props.addDefaultSrc}
         />
--- tvmv-react-app/src/components/TVContent/SingleShow.js
+++ tvmv-react-app/src/components/TVContent/SingleShow.js
@@ -56,13 +56,12 @@ class SingleShow extends Component {
       .then(res => this.setState({ episodes: res.data.episodes }))
       .catch(err => console.log(err));
 
-      //---- FAVORITE SECTION -----//
-      if (!localStorage.favoriteShows) {
-        let favoriteShows = []
-        favoriteShows.push(JSON.parse(localStorage.getItem('favoriteShows')));
-        localStorage.setItem('favoriteShows', JSON.stringify(favoriteShows));
-      }
-     
+    //---- FAVORITE SECTION -----//
+    if (!localStorage.favoriteShows) {
+      let favoriteShows = [];
+      favoriteShows.push(JSON.parse(localStorage.getItem('favoriteShows')));
+      localStorage.setItem('favoriteShows', JSON.stringify(favoriteShows));
+    }
   }
 
   selectSeason = number => {
@@ -83,20 +82,31 @@ class SingleShow extends Component {
   addShowtoLocalFavorites = (id, poster_path) => {
     // grabs current items and spreads them to a new array. Add new item after it.
     let favoriteShows = JSON.parse(localStorage.getItem('favoriteShows'));
-    favoriteShows.push({id, poster_path})
+    favoriteShows.push({ id, poster_path });
     // removes any null in array
-    const filteredShows = favoriteShows.filter(item => item !== null)
+    const filteredShows = favoriteShows.filter(item => item !== null);
+    const result = [];
+    const map = new Map();
+    for (const item of filteredShows) {
+      if (!map.has(item.id)) {
+        map.set(item.id, true);
+        result.push({
+          id: item.id,
+          poster_path: item.poster_path
+        });
+      }
+    }
 
-    localStorage.setItem('favoriteShows', JSON.stringify(filteredShows))
+    localStorage.setItem('favoriteShows', JSON.stringify(result));
 
-      /**
+    /**
      * To grab items, you must first JSON.parse(localStorage.getItem('favoriteShows'))
      *  Then you can add it to a new variable.
      *          const favoriteShows = JSON.parse(localStorage.getItem('favoriteShows'))
      *  */
 
     // console log local storage to see what's inside
-    console.log(JSON.parse(localStorage.favoriteShows))
+    console.log(JSON.parse(localStorage.favoriteShows));
   };
 
   // -----------------------------EPISODES---------------------------------- //
@@ -123,42 +133,45 @@ class SingleShow extends Component {
     const addDefaultSrc = this.props.addDefaultSrc;
     //--------------END OF DECONSTRUCTING-------------------//
     return (
-      <div className='single-page-containter'>
+      <div className="single-page-containter">
         {/* //--------------HERO SECTION-------------------// */}
         <img
           src={back}
-          className='hero-back'
-          alt='back'
+          className="hero-back"
+          alt="back"
           onClick={this.props.history.goBack}
         />
-        <img src={star} 
-        className='hero-star' 
-        alt='star' 
-        onClick={()=>(
-          this.addShowtoLocalFavorites(
-            this.state.show.id, this.state.show.poster_path
+        <img
+          src={star}
+          className="hero-star"
+          alt="star"
+          onClick={() =>
+            this.addShowtoLocalFavorites(
+              this.state.show.id,
+              this.state.show.poster_path
             )
-        )}/>
-        <div className='single-page-hero-info'>
-          <h1 className='single-page-hero-title'>{show.name}</h1>
-          <div className='movie-details'>
-            <p className='movie-details-text'>
+          }
+        />
+        <div className="single-page-hero-info">
+          <h1 className="single-page-hero-title">{show.name}</h1>
+          <div className="movie-details">
+            <p className="movie-details-text">
               {moment(show.last_air_date, 'YYYY-MM-DD').format('MMMM Do YYYY')}
             </p>
             <p>|</p>
-            <p className='movie-details-text'>{show.vote_average}</p>
+            <p className="movie-details-text">{show.vote_average}</p>
             <p>|</p>
-            <p className='movie-details-text'>
+            <p className="movie-details-text">
               {show.number_of_seasons} seasons
             </p>
             <p>|</p>
-            <p className='movie-details-text'>
+            <p className="movie-details-text">
               {show.number_of_episodes} episodes
             </p>
           </div>
-          <p className='single-page-hero-desc'>{show.overview}</p>
-          <Link to={`/show/${this.state.id}/1/1`} className='button-links'>
-            <button className='watch-movie'>Start watching</button>
+          <p className="single-page-hero-desc">{show.overview}</p>
+          <Link to={`/show/${this.state.id}/1/1`} className="button-links">
+            <button className="watch-movie">Start watching</button>
           </Link>
         </div>
         <img
@@ -168,43 +181,43 @@ class SingleShow extends Component {
               ? 'https://www.sheknows.com/wp-content/uploads/2019/02/game-of-thrones-character-posters-FI.jpg'
               : 'http://image.tmdb.org/t/p/original' + show.backdrop_path
           }
-          className='full-hero'
+          className="full-hero"
           alt={show.title}
         />
-        <div className='tv-select-container'>
-          <h1 className='section'>Episodes</h1>
-          <h1 className='section seasons-header'>
+        <div className="tv-select-container">
+          <h1 className="section">Episodes</h1>
+          <h1 className="section seasons-header">
             Season {this.state.selectedSeason}{' '}
           </h1>
         </div>
-        <div className='seasons-flex'>
+        <div className="seasons-flex">
           {seasons.map(season => (
             <p
-              className='seasons'
+              className="seasons"
               onClick={() => this.selectSeason(season.season_number)}
             >
               {season.season_number}
             </p>
           ))}
         </div>
-        <div className='episodes-container'>
+        <div className="episodes-container">
           {episodes.map(episode => (
             <>
-              <div className='episode-container'>
+              <div className="episode-container">
                 <Link
                   to={`/show/${this.state.id}/${this.state.selectedSeason}/${
                     episode.episode_number
                   }`}
-                  className='links'
+                  className="links"
                 >
                   <img
                     src={'http://image.tmdb.org/t/p/w500' + episode.still_path}
-                    className='episode-photo'
+                    className="episode-photo"
                     alt={episode.name}
                   />
-                  <div className='episode-info-flex'>
-                    <p className='episode-number'>{episode.episode_number}.</p>
-                    <p className='episode-name'>{episode.name}</p>
+                  <div className="episode-info-flex">
+                    <p className="episode-number">{episode.episode_number}.</p>
+                    <p className="episode-name">{episode.name}</p>
                   </div>
                 </Link>
               </div>
